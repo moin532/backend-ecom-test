f5eb54ea2580a98f126d87d92f4639a7af093e5b
Mohammed Moinuddin <122919446+moin532@users.noreply.github.com>
2023-10-23 21:16:40 +0700

    Update OrderRoute.js

--- backend/routes/OrderRoute.js
+++ backend/routes/OrderRoute.js
@@ -5,7 +5,7 @@ const {isAuthenticatedUser,authorizeRoles} = require('../middleware/auth');
 
 router.route('/order/new').post(isAuthenticatedUser,newOrder);
 router.route('/order/:id').get(isAuthenticatedUser,getSingleOrder);
-router.route('/orders/me').get(isAuthenticatedUser,myOrders);
+router.route('/orders/me').get(myOrders);
 router.route('/admin/orders').get(isAuthenticatedUser,authorizeRoles("admin"),allOrdersAdmin);
 router.route('/admin/order/:id').put(isAuthenticatedUser,authorizeRoles("admin"),UpdateOrder ).delete(isAuthenticatedUser,
 authorizeRoles("admin"),dltorder);
