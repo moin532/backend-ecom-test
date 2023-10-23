3f73152b0365e40ea5202697f2bf633287059ea0
Mohammed Moinuddin <122919446+moin532@users.noreply.github.com>
2023-10-23 20:17:27 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(fileupload());
 app.use(cors({
-    origin:'http://localhost:3000 ',
+    origin:'true',
     methods:["GET","PUT","POST","DELETE"],
      credentials: true,
 }));
