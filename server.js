273d7997208bb14cde9b758a2ab70d66133ab936
Mohammed Moinuddin <122919446+moin532@users.noreply.github.com>
2023-10-23 18:10:03 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 
 const app  = require('./backend/app');
 const connectdatabase = require('./backend/config/database');
-const cloudinary = require("cloudinary")
+const cloudinary = require("cloudinary");
+const cors = require('cors');
 
-
+app.use(cors({
+    origin:'http://localhost:3000 ',
+    methods:["GET","PUT","POST","DELETE"],
+     credentials: true,
+}));
  
 //config
 if(process.env.NODE_ENV!=="PRODUCTION"){
