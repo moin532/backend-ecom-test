aaa3cfe2ccf9dbf296e329dddb20d57cf442cbbe
Mohammed Moinuddin <122919446+moin532@users.noreply.github.com>
2023-10-24 12:56:47 +0700

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken')
 const User = require('../models/UserModel')
 
 exports.isAuthenticatedUser = catchAsyncErrors(async (req,res,next)=>{
-    const {token} = req.cookies['token'];
+    const { token } = req.cookies['token']
     // console.log(token)
 
     if(!token){
