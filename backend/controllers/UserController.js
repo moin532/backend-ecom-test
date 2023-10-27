7646d240217d28ff56d910933d7d724fa3357356
Mohammed Moinuddin <122919446+moin532@users.noreply.github.com>
2023-10-28 00:14:53 +0700

    Update UserController.js

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -61,7 +61,7 @@ exports.logout = catchasyncerrors(async (req, res, next) => {
    
    res.status(200).cookie("token", null, {
     expires: new Date(Date.now()),
-      httpOnly:true,
+      httpOnly:false,
       
   }).json({
     succes: true,
