4162dea80b81d86646b9eca025f54e85b90855f0
Mohammed Moinuddin <122919446+moin532@users.noreply.github.com>
2023-10-28 00:08:11 +0700

    Update jwtToken.js

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -8,8 +8,9 @@ const sendToken = (user,statusCode,res)=>{
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE  * 24 * 60 *  60 *1000
         ),
-        httpOnly:true,
-        sameSite:"none"
+        httpOnly:false,
+        sameSite:'none'
+        secure: true,
     };
     res.status(statusCode).cookie('token',token,options).json({
         succes:true,
